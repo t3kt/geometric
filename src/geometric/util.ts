74247ce7d9596270b2ea24993fbc681222d7b053
tekt <tekt@optexture.com>
2018-09-22 00:25:54 -0700

    got the basic rhombus generator working

--- src/geometric/util.ts
+++ src/geometric/util.ts
@@ -138,23 +138,16 @@ export function createRhombusFromCorners(
   attrs?) : paper.Path {
   const diff1to2 = pt2.subtract(pt1);
   const diff2to3 = pt3.subtract(pt2);
-  const sideLength = diff1to2.length;
-  if (Math.abs(sideLength - diff2to3.length) > 0.00001) {
-    throw new Error('Side lengths do not match (' + sideLength + ' != ' + diff2to3.length + ')');
+  if (Math.abs(diff1to2.length - diff2to3.length) > 0.00001) {
+    throw new Error('Side lengths do not match (' + diff1to2.length + ' != ' + diff2to3.length + ')');
   }
-  const angleA = diff2to3.angle - diff1to2.angle;
-  const angleB = 180 - angleA;
-  const pt4 = pt1.clone();
-  pt4.x += sideLength * Math.cos(toRadians(angleB));
-  pt4.y += sideLength * Math.sin(toRadians(angleB));
-  const polyPoints = [
-    pt1.clone(),
-    pt2.clone(),
-    pt3.clone(),
-    pt4
-  ];
   const poly = new paper.Path({
-    segments: polyPoints,
+    segments: [
+      pt1.clone(),
+      pt2.clone(),
+      pt3.clone(),
+      pt3.subtract(diff1to2),
+    ],
     strokeColor: '#00ff00',
     closed: true
   });
